c09a6d138ebf2ae98b38ca87a85485278a34a6f2
dimahike <dimahike@gmail.com>
2020-10-08 13:04:14 +0300

    Updates

---
+++ precache-manifest.8ed8525d9a9080ee77b4ad5a38051a62.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e56691f216a4cbb8959abf5229636107",
+    "url": "/react-store-2/index.html"
+  },
+  {
+    "revision": "386291f7057f7c9ec5fc",
+    "url": "/react-store-2/static/css/main.eb6effff.chunk.css"
+  },
+  {
+    "revision": "c4609aa59fed583a1cd9",
+    "url": "/react-store-2/static/js/2.79b67c52.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-store-2/static/js/2.79b67c52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "386291f7057f7c9ec5fc",
+    "url": "/react-store-2/static/js/main.41456e46.chunk.js"
+  },
+  {
+    "revision": "c55956f1866346df81d3",
+    "url": "/react-store-2/static/js/runtime-main.30a9d067.js"
+  },
+  {
+    "revision": "ef7a4e2faddc1b0c6e08720313d9dd6a",
+    "url": "/react-store-2/static/media/product1.ef7a4e2f.jpg"
+  },
+  {
+    "revision": "f2269ca310a7e764b9a3f53e2f516ba5",
+    "url": "/react-store-2/static/media/product2.f2269ca3.jpg"
+  },
+  {
+    "revision": "f38d46f88b13437fd8cc544b0050bee1",
+    "url": "/react-store-2/static/media/product3.f38d46f8.jpg"
+  },
+  {
+    "revision": "27c751aa6be28b99eab5539acbfae960",
+    "url": "/react-store-2/static/media/product4.27c751aa.jpg"
+  },
+  {
+    "revision": "20531784e89856faf0a5023b54aa3e08",
+    "url": "/react-store-2/static/media/product5.20531784.jpg"
+  },
+  {
+    "revision": "ec58ad659f2cd537e2e9a71b08088e59",
+    "url": "/react-store-2/static/media/product6.ec58ad65.jpg"
+  },
+  {
+    "revision": "d638c6d57103a9d6c95f2ace4cb29fa4",
+    "url": "/react-store-2/static/media/product7.d638c6d5.jpg"
+  },
+  {
+    "revision": "c43504d088a9e2d690688e65ecc991a0",
+    "url": "/react-store-2/static/media/product8.c43504d0.jpg"
+  },
+  {
+    "revision": "6ca3d7348cd2a73e40884cfe876c18f9",
+    "url": "/react-store-2/static/media/product9.6ca3d734.jpg"
+  }
+]);
