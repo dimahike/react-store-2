3ca6e160146f11ba09623d3d7e9a85ec99586e47
dimahike <dimahike@gmail.com>
2020-10-08 15:18:25 +0300

    fix footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,27 +1,29 @@
-import React from 'react'
+import React from 'react';
 
 function Footer() {
-    return (
-        <div id="footer">
-        <div>
-          <div className="contacts">
-            <h3>Connect With Me </h3>
-            <div className="social-links-footer">
-              <span>
-                <i class="fab fa-github"></i>
-              </span>
-              <span>
-                <i class="fab fa-linkedin-in"></i>
-              </span>
-              <span>
-                <i class="fab fa-telegram-plane"></i>
-              </span>
-            </div>
+  const year = new Date().getFullYear();
+
+  return (
+    <div id="footer">
+      <div>
+        <div className="contacts">
+          <h3>Connect With Me </h3>
+          <div className="social-links-footer">
+            <span>
+              <i class="fab fa-github"></i>
+            </span>
+            <span>
+              <i class="fab fa-linkedin-in"></i>
+            </span>
+            <span>
+              <i class="fab fa-telegram-plane"></i>
+            </span>
           </div>
-          <p className="copyright">© Copyright YOUR WEATHER, 2020</p>
         </div>
+        <p className="copyright">© Copyright Simple Store, {year}</p>
       </div>
-    )
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
